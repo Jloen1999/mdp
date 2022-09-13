8bc16db6a574ee6a02e35206462ee1fc99bb6eb9
jloen1999 <jloelananguan@gmail.com>
2022-09-13 13:51:09 +0200

    new commit

---
+++ pruebaProyecto/src/es/unex/cum/mdp/sesion0/Reloj.java
@@ -0,0 +1,35 @@
+package es.unex.cum.mdp.sesion0;
+
+
+
+
+/*
+Cada alumno deberá implementar una clase Reloj compuesta por los siguientes atributos: int horas; int minutos; int segundos.
+Además debe tener los siguientes métodos (CUIDADO: deben llamarse tal y como aparecen a continuación):
+
+• Reloj()
+• Reloj(int hora, int minutos, int segundos)
+• int getHoras()
+• void setHoras(int hora)
+• int getMinutos()
+• void setMinutos(int minutos)
+• int getSegundos()
+• void setSegundos(int segundos)
+• boolean equals(Reloj obj)
+• String toString() deberá devolver la información en el siguiente formato:
+◦
+"Reloj [horas=" + horas + ", minutos=" + minutos + ", segundos=" + segundos + "]";
+• void incrementarSegundos(): Incrementa en uno los segundos
+• void incrementarMinutos() : Incrementa en uno los minutos
+• void incrementarHoras(): : Incrementa en uno las horas
+• void decrementarSegundos(): Decrementa en uno los segundos
+• void decrementarMinutos(): Decrementa en uno los minutos
+• void decrementarHoras(): Decrementa en uno las horas
+• long getTiempo(): Devuelve en número entero las horas, minutos y segundos.
+• void setTiempo(long s): Asigna las horas, minutos y segundos en base a un número long
+• void anadirTiempo(Reloj r): Añade al reloj actual, la información de otro reloj
+• void restarTiempo(Reloj r): Resta al reloj actual, la información de otro reloj
+*/
+public class Reloj {
+
+}
